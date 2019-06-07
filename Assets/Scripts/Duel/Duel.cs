ab6497cafa0c9009ea888465ce4868b42579e19f
yuer <sunnyuer@qq.com>
2019-06-07 16:57:52 +0800

    决斗阶段

--- Assets/Scripts/Duel/Duel.cs
+++ Assets/Scripts/Duel/Duel.cs
@@ -1,6 +1,7 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using System.IO;
+using System.Threading;
 using UnityEngine;
 using UnityEngine.UI;
 
@@ -9,21 +10,35 @@ public class Duel : MonoBehaviour
     public GameObject mainLayout;
     public GameObject deckOwn;
     public GameObject deckOps;
-    public static List<string> owndeck = new List<string>();
-    public static List<string> ownextra = new List<string>();
-    public static List<string> opsdeck = new List<string>();
-    public static List<string> opsextra = new List<string>();
+    public static List<string> owndeck;
+    public static List<string> ownextra;
+    public static List<string> opsdeck;
+    public static List<string> opsextra;
     public static Sprite UIMask;
     public static CardSpriteManager spriteManager;
+    public GameObject endTurnButton;
+    public Text phaseText;
+    public GameObject mainPhaseButton;
+    public static int whoTurn;
+    public static int duelPhase;
 
     // Start is called before the first frame update
     void Start()
     {
+        owndeck = new List<string>();
+        ownextra = new List<string>();
+        opsdeck = new List<string>();
+        opsextra = new List<string>();
         spriteManager = new CardSpriteManager();
         UIMask = GameObject.Find("DeckImageOwn").GetComponent<Image>().sprite;//保存UIMask
+        //读取卡组
         ReadDeckFile();
+        //放置卡组
         deckOwn.GetComponent<DeckOwn>().DeckUpdate();
         deckOps.GetComponent<DeckOps>().DeckUpdate();
+        //初始化回合和阶段
+        whoTurn = 0;
+        ChangePhase(0);
     }
 
     // Update is called once per frame
@@ -55,6 +70,84 @@ public class Duel : MonoBehaviour
             ownextra.Add(strs[i]);
             opsextra.Add(strs[i]);
             i++;
+        }
+    }
+
+    public void ChangePhase(int phase)
+    {
+        if (phase >= 7)
+        {
+            phase = 0;
+            whoTurn = 1 - whoTurn;
+        }
+        duelPhase = phase;
+        PhaseButtonShow();
+        if (duelPhase == 0)
+        {
+            if (whoTurn == 0) phaseText.text = "我的回合";
+            else phaseText.text = "对方回合";
+            StartCoroutine(PhaseWait());
+        }
+        if (duelPhase == 1)
+        {
+            phaseText.text = "抽卡阶段";
+            StartCoroutine(PhaseWait());
+        }
+        if (duelPhase == 2)
+        {
+            phaseText.text = "准备阶段";
+            StartCoroutine(PhaseWait());
+        }
+        if (duelPhase == 3)
+        {
+            phaseText.text = "主一阶段";
+        }
+        if (duelPhase == 4)
+        {
+            phaseText.text = "战斗阶段";
+        }
+        if (duelPhase == 5)
+        {
+            phaseText.text = "主二阶段";
+        }
+        if (duelPhase == 6)
+        {
+            phaseText.text = "结束阶段";
+            StartCoroutine(PhaseWait());
+        }
+    }
+
+    IEnumerator PhaseWait()
+    {
+        yield return new WaitForSeconds(1);
+        ChangePhase(++duelPhase);
+    }
+
+    public void PhaseButtonShow()
+    {
+        if (duelPhase >= 3 && duelPhase <= 4) mainPhaseButton.SetActive(true);
+        else mainPhaseButton.SetActive(false);
+        if (duelPhase >= 3 && duelPhase <= 5) endTurnButton.SetActive(true);
+        else endTurnButton.SetActive(false);
+    }
+
+    public void OnEndTurnButtonClick()
+    {
+        ChangePhase(6);
+    }
+
+    public void OnMainPhaseButtonClick()
+    {
+        Text buttonText = mainPhaseButton.GetComponentInChildren<Text>();
+        if (duelPhase == 4)
+        {
+            buttonText.text = "开始战斗";
+            ChangePhase(5);
+        }
+        if (duelPhase == 3)
+        {
+            buttonText.text = "结束战斗";
+            ChangePhase(4);
         }
     }
 
